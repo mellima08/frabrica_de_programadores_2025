16684b5b3b22ee47df10149cfc7ee164d4ed775b
mellima08 <ribeiromelissa2008@gmail.com>
2025-05-08 15:51:38 -0300

    Add files via upload
    FUNÇÃO

---
+++ funções.py
@@ -0,0 +1,32 @@
+texto_escrito="Esse livro é legal!"
+outro_texto='Vou aprender a progamar.'
+outro_texto=texto_escrito
+#print(outro_texto)
+primeiro_nome="Melissa"
+segundo_nome="Ribeiro"
+nome_completo=primeiro_nome + segundo_nome
+#print(nome_completo)
+primeiro_nome="Melissa"
+sausacao="OLá" + primeiro_nome
+#print(sausacao)
+balas=5
+quantidade_balas=40
+quantidade_balas *= balas
+#print(quantidade_balas) 
+balas=5
+quantidade_balas=40
+quantidade_balas-= balas
+#print(quantidade_balas)
+#nome=input()
+#saudacao= "OLá" + nome
+#print(saudacao)
+
+numero_string=input()
+numero_inteiro=int( numero_string)
+subitracao= numero_inteiro - 2
+numero_um=input()
+numero_dois=input()
+resultado= int(numero_um) * int(numero_dois)
+resultado_texto= str(resultado)
+mensagem= "O resultado da soma é" + resultado_texto
+print(resultado_texto)
