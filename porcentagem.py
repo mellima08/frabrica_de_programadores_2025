c0d8b5ab9b7dfddcee17f0dde63aa77bfd5e8d28
mellima08 <ribeiromelissa2008@gmail.com>
2025-05-16 14:44:03 -0300

    Add files via upload

---
+++ porcentagem.py
@@ -0,0 +1,19 @@
+#valor_total= float(input("digite_valor_tota"))
+#porcentagem=1.3
+#valor_parte= valor_total * (porcentagem / 100)
+#print(valor_parte)
+#print(valor_parte + valor_total)
+
+#  exercicio 4
+
+#Celsius= float(input("digite celsius"))
+#F= (9 * Celsius + 160) /5
+#print("Temperatura:", F)
+
+# exercicio 18
+valor1=int(input("digite primeiro valor:"))
+valor2=int(input("digite segundo valor:"))
+if valor1> valor2:
+    print( "A diferencia entre o maior e o menor valor é: ", valor1 - valor2)
+else:
+    print( "A diferencia entre o maior e o menor valor é: ", valor2 - valor1)
