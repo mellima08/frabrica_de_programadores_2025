d7a030596ef0abb2dc3081c9b7dcbb319b683536
mellima08 <ribeiromelissa2008@gmail.com>
2025-05-26 15:02:42 -0300

    Add files via upload

---
+++ roundrobin.py
@@ -0,0 +1,16 @@
+saudação= input("Boas vindas á pizzaria")
+Sabor1= "Toscana"
+Sabor2= "Muçarela"
+Sabor3= "Marguerita"
+Sabor4= "Calabresa"
+pedido=int(input("Digite qual pizza que escolheu"))
+
+ValorSabor1 =59.99  #Toscana"
+ValorSabor2 =54.99 #Muçarela"
+ValorSabor3 =57.99  #Marguerita" 
+ValorSabor4 = 52.99 #Calabresa" 
+nome_do_cliente=("digite o nome do cliente")
+
+from datetime import date
+data_atual= date.today()
+print(data_atual)
